75060d63891074f90fb4473e5bd6f01252b01e31
mz-mjoo <81146603+mz-mjoo@users.noreply.github.com>
2022-06-19 17:10:20 +0900

    storybook main.js 설정 변경

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,26 +1,28 @@
-import path from 'path';
-const toPath = (filePath) => path.join(pocess.cwd(), filePath);
-
-export default {
+const path = require('path');
+const toPath = (filePath) => path.join(process.cwd(), filePath);
+const {TsconfigPathsPlugin} = require('tsconfig-paths-webpack-plugin');
+module.exports = {
   stories: [
-    '../stories/**/*.stories.mdx',
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+    '../components/**/*.stories.mdx',
+    '../components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    'storybook-addon-next-router',
   ],
-  framework: '@storybook/react',
   core: {
-    builder: '@storybook/builder-webpack5',
+    builder: 'webpack5',
   },
   webpackFinal: async (config) => {
-    config.resolve.plugins.push(new TsconfigPathsPlugin({}));
+    const tsconfigPathsPlugin = new TsconfigPathsPlugin({});
     return {
       ...config,
       resolve: {
         ...config.resolve,
+        plugins: config.resolve.plugins
+          ? [...config.resolve.plugins, tsconfigPathsPlugin]
+          : [tsconfigPathsPlugin],
         alias: {
           ...config.resolve.alias,
           '@emotion/core': toPath('node_modules/@emotion/react'),
